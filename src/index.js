2ecba20a80101bc1a214797ebdc7fdca87c7503e
Ciri Ryan <cirijaye@yahoo.com>
2022-02-26 07:09:15 -0800

    add map and pin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 // Initialize and add the map
+// 41.8781° N, 87.6298° W chicago
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2lyaWpheWUiLCJhIjoiY2wwMzVhOG51MWFvajNqcGtoNjY1NXY3biJ9.d-81uebJHiFJDLiYUCXJng';
+let map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-87.6298, 41.878],
+  zoom: 11.15 
+});
+
+map.on('load', ()=>{
+    render(data)
+})
+
+onpointClick = (e)=> {
+//mangle data
+render(data)
+}
+
+let el = document.getElementById('id')
+el.addEventListener('click')
 
 
-sites = [
-    {
-        'place': 'University of Chicago',
-        'longitude': 041.788808, 
-        'latitude': -87.598713
-    },
-    
+// 1. function render()
+// clears the map.
+// list of five points (objects dexrtions)
 
-]
